020a686cf930c62d1284c61b67650450df4cfc2b
AbhishekKapoor <kapoor.ab@gmail.com>
2015-02-21 18:53:50 +0530

    Second Commit

--- R/MaxDiff.R
+++ R/MaxDiff.R
@@ -1,9 +1,12 @@
-design <- function (attributes,sets){
+design <- function (attributes,sets,samplesize){
   
+  if (sets > attributes/2) 
+    stop ("We recommend displaying either 4 or 5 sets to each respondents.However, we don't recommend
+          sets greater than half of the attributes")
+  samplesize<- version
   lowerlimit<- 2* attributes/sets
   ideal<- 3* attributes/sets
   upperlimit<- 4* attributes/sets
-  
-  d<- tradeoff.des 
+  des<- tradeoff.des (attributes,sets)
   
 }
